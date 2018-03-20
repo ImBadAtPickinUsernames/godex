0685e4c111d79fcdddcf665ca04aec6411935e6f
Dominik Lopez-Caperuchipi <darudezedstorm1@gmail.com>
2018-03-20 16:29:45 +0100

    - detail screen contains now data about the pokemon - added launcher icon

--- app/src/main/java/com/imbadatpickindomains/godex/PokemonDetailView.kt
+++ app/src/main/java/com/imbadatpickindomains/godex/PokemonDetailView.kt
@@ -21,7 +21,37 @@ class PokemonDetailView: AppCompatActivity() {
     }
 
     private fun fillPage(pokemonData: List<Pokemon>, dexNum:Int) {
+        val name = "p" + dexNum.inc()
+        val shinyName = "s" + dexNum.inc()
+        val resID = resources.getIdentifier(name, "drawable", packageName)
+        val resIDShiny = resources.getIdentifier(shinyName, "drawable", packageName)
+
         pokemonName.text = pokemonData[dexNum].name
+
+        // pictures
+        imgPokemon.setImageResource(resID)
+        imgPokemonShiny.setImageResource(resIDShiny)
+        // text - stats
+        attackValue.text = pokemonData[dexNum].stats.baseAttack.toString()
+        defenceValue.text = pokemonData[dexNum].stats.baseDefense.toString()
+        staminaValue.text = pokemonData[dexNum].stats.baseStamina.toString()
+        progressBar.max = 300
+        progressBar.progress = pokemonData[dexNum].stats.baseAttack
+        progressBar2.max = 396
+        progressBar2.progress = pokemonData[dexNum].stats.baseDefense
+        progressBar3.max = 510
+        progressBar3.progress = pokemonData[dexNum].stats.baseStamina
+
+        for (item in pokemonData[dexNum].quickMoves){
+            movesetsFast.append(item.name + "\n")
+        }
+
+        for (item in pokemonData[dexNum].cinematicMoves){
+            movesetsCharge.append(item.name + "\n")
+        }
+
+        buddyDistanceValue.text = pokemonData[dexNum].kmBuddyDistance.toString()
+
     }
 
 }
